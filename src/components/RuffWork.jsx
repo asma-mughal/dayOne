4b1ae04999c5ad5394084eb4b15836770c9ccbf7
unknown <asmamughal097@gmail.com>
2023-08-27 18:51:23 +0500

    challangeTwoa

--- src/components/RuffWork.jsx
+++ src/components/RuffWork.jsx
@@ -1,22 +1,15 @@
 import React from 'react'
 
 const RuffWork = () => {
-  function performOperation(secondInteger, secondDecimal, secondString) {
-    const firstInteger = 4;
-    const firstDecimal = 4.0;
-    const firstString = 'HackerRank ';
-    
-    // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
-    console.log(firstInteger + parseInt(secondInteger))
-    console.log(parseFloat(firstDecimal) + secondDecimal);
-    console.log(firstString + secondString);
-    
-    // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
-    
+const getArea =(length, width) =>{
+console.log(length * width);
+}
+const getPerimeter = (length, width) =>{
+ console.log(2 * (length + width))
 }
   return (
     <div>
-      <button onClick={()=> performOperation("5",6.0,"Heloow")}>Click to Perform</button>
+      <button onClick={()=> getPerimeter(3,4)}>Click to Perform</button>
     </div>
   )
 }
