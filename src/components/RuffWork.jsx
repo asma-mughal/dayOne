b0f11f7e2cd88b9390b5622a62518bd3bcffcd22
unknown <asmamughal097@gmail.com>
2023-08-27 18:46:48 +0500

    challangeTwo

--- src/components/RuffWork.jsx
+++ src/components/RuffWork.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 
 const RuffWork = () => {
-    function init(name)
-     { // name is a local variable created by init
-        function displayName() {
-          // displayName() is the inner function, that forms the closure
-          console.log(name); // use variable declared in the parent function
-        }
-        displayName();
-      }
-      init("apple");
+  function performOperation(secondInteger, secondDecimal, secondString) {
+    const firstInteger = 4;
+    const firstDecimal = 4.0;
+    const firstString = 'HackerRank ';
+    
+    // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
+    console.log(firstInteger + parseInt(secondInteger))
+    console.log(parseFloat(firstDecimal) + secondDecimal);
+    console.log(firstString + secondString);
+    
+    // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+    
+}
   return (
     <div>
+      <button onClick={()=> performOperation("5",6.0,"Heloow")}>Click to Perform</button>
     </div>
   )
 }
