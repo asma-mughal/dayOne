2b4f41759b7a76b8b053fcde92360a2b5d70cb6c
unknown <asmamughal097@gmail.com>
2023-08-28 13:56:11 +0500

    another challeneg

--- src/components/RuffWork.jsx
+++ src/components/RuffWork.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
 
 const RuffWork = () => {
-const getArea =(length, width) =>{
-console.log(length * width);
-}
-const getPerimeter = (length, width) =>{
- console.log(2 * (length + width))
+function getGrade(score) {
+    let grade;
+    if(score> 25 && score<=30){
+    grade = 'A';
+    }
+    else if(score>20 && score <=25 ){
+      grade = 'B';
+    }
+    else if(score > 15 && score <= 20){
+     grade = 'C';
+    }
+    else if(score > 10 && score <= 15){
+      grade ='D'
+    }
+    else if(score > 5 && score <=10 ){
+      grade = 'E'
+    }
+    else {
+      grade = 'F'
+    }
+    
+    return grade;
 }
   return (
     <div>
-      <button onClick={()=> getPerimeter(3,4)}>Click to Perform</button>
+      <button onClick={()=>{
+      const res = getGrade(13)
+      console.log(res)
+      }}>Click to Perform</button>
     </div>
   )
 }
